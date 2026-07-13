7ffbc01397576ee66430b603c0c2c5934444f523
Jan T. Sott <git@idleberg.com>
2026-07-13 22:03:22 +0200

    chore: update meta data

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,16 @@ export default defineConfig({
 	integrations: [
 		castro({
 			title: 'AVS Forum Archive',
+			description: 'Mirror of the AVS forum on Winamp.com',
+			keywords: [
+				'AVS Presets',
+				'avs',
+				'Advanced Visualization Studio',
+				'winamp',
+				'wvs',
+				'Winamp Visualization Studio',
+				'audio visualization',
+			],
 			githubPages: true,
 		}),
 		sitemap(),
