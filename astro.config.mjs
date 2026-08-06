4b24320ad1ceb3353dba907bb5b7bd1ea3076e98
Jan T. Sott <git@idleberg.com>
2026-08-07 00:07:56 +0200

    style: change default font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,21 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import castro from './.castro/integration.ts';
 
 export default defineConfig({
 	integrations: [
 		castro({
-			title: 'AVS Forum Archive',
-			description: 'Mirror of the AVS forum on Winamp.com',
-			keywords: [
-				'AVS Presets',
-				'avs',
-				'Advanced Visualization Studio',
-				'winamp',
-				'wvs',
-				'Winamp Visualization Studio',
-				'audio visualization',
-			],
+			title: 'Forum Archive',
 			githubPages: true,
 		}),
 		sitemap(),
+	],
+	fonts: [
+		{
+			provider: fontProviders.fontsource(),
+			name: 'Libre Franklin',
+			cssVariable: '--font-sans',
+		},
 	],
 });
